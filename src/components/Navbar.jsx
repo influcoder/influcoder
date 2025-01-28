683cdd6d9928dad353f5b512ed417165d4f763db
Vaishali nile <vaishalinile896@gmail.com>
2025-01-28 19:18:05 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="flex justify-evenly">
+      <a href="">Home</a>
+      <a href="">Projects</a>
+      <a href="">About us</a>
+      <a href="">Contact Us</a>
+    </div>
+  );
+}
+
+export default Navbar;
